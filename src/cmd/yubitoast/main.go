b5b03151717769d43382e4df7c5468ef619ef771
Ronald <ronald@provablelabs.com>
2019-10-11 14:35:42 +0200

    Add -popup flag to show popup instead of notification

--- src/cmd/yubitoast/main.go
+++ src/cmd/yubitoast/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"regexp"
+	"runtime"
+	"os/exec"
 
 	"github.com/pkg/errors"
 
@@ -16,6 +18,9 @@ var pkSignRegexp = regexp.MustCompile("PKSIGN --hash=.+ OPENPGP\\.1$")
 
 var fLogfile = flag.String("logfile", "/var/log/gpg-agent.log", "path to gpg-agent.log")
 var fVerbose = flag.Bool("verbose", false, "verbose logging")
+var fPopup = flag.Bool("popup", false, "show popup (if supported)")
+
+const popupTimeout = 10 // seconds
 
 func main() {
 	flag.Parse()
@@ -32,6 +37,16 @@ func main() {
 		panic(errors.Wrapf(err, "tail gpg-agent log [%s]", (fLogfile)))
 	}
 
+	usePopup := false
+	if *fPopup {
+		switch {
+		case runtime.GOOS == "darwin":
+			usePopup = true
+		default:
+			fmt.Println("-popup not supported on", runtime.GOOS)
+		}
+	}
+
 	// now we wait
 	for line := range t.Lines {
 		if *fVerbose {
@@ -39,13 +54,26 @@ func main() {
 		}
 
 		if pkAuthRegexp.Match([]byte(line.Text)) {
-			showNotification("Authenticate")
+			showNotification("Authenticate", usePopup)
 		} else if pkSignRegexp.Match([]byte(line.Text)) {
-			showNotification("Sign")
+			showNotification("Sign", usePopup)
 		}
 	}
 }
 
-func showNotification(msg string) {
-	notify.Notify("Yubikey Touch Requested", "", msg, "gnupg.png")
+func showNotification(msg string, usePopup bool) {
+	appName := "Yubikey Touch Requested"
+
+	switch {
+	case runtime.GOOS == "darwin" && usePopup:
+		arg := fmt.Sprintf("tell app \"System Events\" to display dialog \"%s\" buttons \"Close\" with title \"%s\" with icon caution giving up after (%d)", appName, msg, popupTimeout)
+		cmd := exec.Command("osascript", "-e", arg)
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println(errors.Wrapf(err, "osascript").Error())
+		}
+	default:
+		notify.Notify("Yubikey Touch Requested", "", msg, "gnupg.png")
+		notify.Notify(appName, "", msg, "gnupg.png")
+	}
 }
